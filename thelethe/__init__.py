5ff5daf561a347e45928fe726bf291dc23e2af91
BREW <81044362+b-re-w@users.noreply.github.com>
2025-05-07 06:05:38 +0900

    Chore: Deploy Config

--- thelethe/__init__.py
+++ thelethe/__init__.py
@@ -1,3 +1,4 @@
+"""theRiverLethe - AI models with adaptive memory management and strategic forgetting, inspired by Greek mythology and neuroscience."""
 from .architectures import *
 #from .datasets import *
 from .utils import *
