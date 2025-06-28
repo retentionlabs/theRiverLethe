156d1d24c6ec75a59b94a7ac407a2f84db8b3358
3GID <km020207@naver.com>
2025-06-28 14:32:40 +0900

    Refactor: Change model name Atlas to Cronos
    Due to Google's recent publication of a paper under the name 'Atlas,' we decided to rename our model. Replaces AtlasConfig with CronosConfig in the usage notebook and updates the adapter name accordingly. This reflects a change in the recommended configuration for the example.

---
+++ thelethe/architectures/titans/models/cronos/__init__.py
@@ -0,0 +1,3 @@
+from .modeling_cronos import CronosPreTrainedTitansModel, CronosModel, CronosForCausalLM
+from .configuration_cronos import CronosConfig
+#from .tokenization_cronos import CronosTokenizer
---
+++ thelethe/architectures/titans/models/cronos/modeling_cronos.py
@@ -0,0 +1,30 @@
+from ...modelings import PreTrainedTitansModel
+from .configuration_cronos import CronosConfig
+
+
+class CronosPreTrainedTitansModel(PreTrainedTitansModel):
+    config_class = CronosConfig
+
+
+class CronosModel(CronosPreTrainedTitansModel):
+    """
+    This is the base class for all Cronos models.
+    It inherits from `CronosPreTrainedTitansModel` and provides the basic functionality for loading and saving model weights.
+    """
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.config = config
+        self.init_weights()  # Initialize weights using the method from PreTrainedTitansModel
+
+
+class CronosForCausalLM(CronosPreTrainedTitansModel):
+    """
+    This is the base class for all Cronos models used for causal language modeling.
+    It inherits from `CronosPreTrainedTitansModel` and provides the basic functionality for loading and saving model weights.
+    """
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.config = config
+        self.init_weights()
